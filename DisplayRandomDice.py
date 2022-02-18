161b8e90f8ee4d13881f05b2efb331e48e56dda5
sebtheman <84299475+Sebastian-Webster@users.noreply.github.com>
2022-02-18 23:26:09 +1300

    DisplayRandomDice.py shows dice on same row

--- DisplayRandomDice.py
+++ DisplayRandomDice.py
@@ -28,29 +28,44 @@
 
 import random
 
-diceValues = []
-diceArrangement = [[], []. [], []]
+lineOne = ''
+lineTwo = ''
+lineThree = ''
+numbers = []
 
 for i in range(6):
-    diceValues.append(random.randint(1,6))
+    randomNumber = random.randint(1,6)
+    numbers.append(randomNumber)
+    if (randomNumber == 1):
+        lineOne += '|' + ' '*7 + '| '
+        lineTwo += '|' + ' '*3 + 'O' + ' '*3 + '| '
+        lineThree += '|' + ' '*7 + '| '
+    elif (randomNumber == 2):
+        lineOne += '|' + 'O' + ' '*6 + '| '
+        lineTwo += '|' + ' '*7 + '| '
+        lineThree += '|' + ' '*6 + 'O| '
+    elif (randomNumber == 3):
+        lineOne += '|' + 'O' + ' '*6 + '| '
+        lineTwo += '|' + ' '*3 + 'O' + ' '*3 + '| '
+        lineThree += '|' + ' '*6 + 'O| '
+    elif (randomNumber == 4):
+        lineOne += '| ' + 'O' + ' '*3 + 'O' + ' ' + '| '
+        lineTwo += '|' + ' '*7 + '| '
+        lineThree += '| ' + 'O' + ' '*3 + 'O' + ' ' + '| '
+    elif (randomNumber == 5):
+        lineOne += '| ' + 'O' + ' '*3 + 'O' + ' ' + '| '
+        lineTwo += '|' + ' '*3 + 'O' + ' '*3 + '| '
+        lineThree += '| ' + 'O' + ' '*3 + 'O' + ' ' + '| '
+    elif (randomNumber == 6):
+        lineOne += '| ' + 'O' + ' '*3 + 'O' + ' ' + '| '
+        lineTwo += '| ' + 'O' + ' '*3 + 'O' + ' ' + '| '
+        lineThree += '| ' + 'O' + ' '*3 + 'O' + ' ' + '| '
 
+print(f'Dice Values: {numbers}')
 
-print('Dice:\n')
-print('Dice Values:', diceValues)
-
-for i in range(6):
-    print('_________')
-    if (diceValues[i] == 1):
-        print('|\t|\n|   O   |\n|\t|')
-    elif (diceValues[i] == 2):
-        print('|O      |\n|\t|\n|      O|')
-    elif (diceValues[i] == 3):
-        print('|O      |\n|   O   |\n|      O|')
-    elif (diceValues[i] == 4):
-        print('|O     O|\n|\t|\n|O     O|')
-    elif (diceValues[i] == 5):
-        print('|O     O|\n|   O   |\n|O     O|')
-    elif (diceValues[i] == 6):
-        print('|O     O|\n|O     O|\n|O     O|')
-    print(chr(8254)*9)
+print('_'*9, '_'*9, '_'*9, '_'*9, '_'*9, '_'*9)
+print(lineOne)
+print(lineTwo)
+print(lineThree)
+print(chr(8254)*9, chr(8254)*9, chr(8254)*9, chr(8254)*9, chr(8254)*9, chr(8254)*9)
 
